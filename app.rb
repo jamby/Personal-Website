7cfa09ac010b0ab353f6cec2aa1f368493477134
Jared Hamby <irjamby@gmail.com>
2012-04-10 23:28:08 -0400

    Some fixes plus attempts at bolding the page you're on.

--- app.rb
+++ app.rb
@@ -2,9 +2,24 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
+  @action = 'index'
   erb :index
 end
 
 get '/about' do
+  @action = 'about'
   erb :about
+end
+
+get '/projects' do
+  @action = 'projects'
+  erb :projects
+end
+
+get '/code' do
+  erb :code
+end
+
+get '/contact' do
+  erb :contact
 end
