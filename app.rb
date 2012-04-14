d7819380997c5588fcde4cdad73493dd6bbacbf2
Jared Hamby <irjamby@gmail.com>
2012-04-13 22:04:52 -0400

    Attempting to put projects into the project page.

--- app.rb
+++ app.rb
@@ -17,9 +17,26 @@ get '/projects' do
 end
 
 get '/code' do
+  @action = 'code'
   erb :code
 end
 
 get '/contact' do
+  @action = 'contact'
   erb :contact
+end
+
+get '/projects/polarity_shift' do
+  @action = 'polarity_shift'
+  erb :projects
+end
+
+get '/projects/the_depths' do
+  @action = 'the_depths'
+  erb :projects
+end
+
+get '/proejcts/peg_game' do
+  @action = 'peg_game'
+  erb :projects
 end
