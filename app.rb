074175e11b868211ee57ee7c2b9e0eef6747c3df
Jared Hamby <irjamby@gmail.com>
2012-04-15 21:09:19 -0400

    Various code samples added along with edited pictures.

--- app.rb
+++ app.rb
@@ -50,3 +50,23 @@ get '/code/peggame_update' do
   @action = 'peggame_update'
   erb :code
 end
+
+get '/code/polarityshift_animload' do
+  @action = 'polarityshift_animload'
+  erb :code
+end
+
+get '/code/polarityshift_animupdate' do
+  @action = 'polarityshift_animupdate'
+  erb :code
+end
+
+get '/code/depths_importer' do
+  @action = 'depths_importer'
+  erb :code
+end
+
+get '/code/depths_levelsystemload' do
+  @action = 'depths_levelsystemload'
+  erb :code
+end
