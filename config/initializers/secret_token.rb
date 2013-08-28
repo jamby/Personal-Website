05094400456c2042c7861df0beba62a90165f292
Jared Hamby <irjamby@gmail.com>
2013-08-28 09:57:50 -0400

    Updating to version 2.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonalWebsite::Application.config.secret_key_base = 'd5db9a70d0246b6f7756dd926cfc9db01fefe3421adf106ee036174af860735f1fe83baa2b45cfa34274fe2a516d2e155e3909d6c3a28e4b0f3751c79115417a'
--- public/javascripts/application.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  if($('.flash')) {
-  setTimeout("$('.flash').fadeIn('slow').delay(1500).fadeOut('slow')", 1000); }
-});
